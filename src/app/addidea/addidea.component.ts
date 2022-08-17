c1823f697576de99a631294cf2d9b358e1461a1a
surya-devarakonda <devarakonda2001@gmail.com>
2022-08-17 15:47:20 +0530

    New one

--- src/app/addidea/addidea.component.ts
+++ src/app/addidea/addidea.component.ts
@@ -10,9 +10,12 @@ import { ResponsesService } from '../responses.service';
 })
 export class AddideaComponent implements OnInit {
 
+
+  //declaring the options that need to be included in the radio button
   selectGender: string = "Male"
   gender: string[] = ['Male', 'Female', 'Transgender', 'Prefer not to say'];
 
+  //declaring the options that need to be included in the drop down
   selectTypes: string = ""
   type: string[] = ['Complaint','Query','Feedback','Suggestion'];
   
@@ -21,6 +24,8 @@ export class AddideaComponent implements OnInit {
 
   ngOnInit(): void {
   }
+// using formgroup in the form for the validations purpose.
+  
   form = new FormGroup({
     name: new FormControl('',[Validators.required, Validators.minLength(5)]),
     body: new FormControl('',[Validators.required, Validators.minLength(50)]),
@@ -37,17 +42,22 @@ export class AddideaComponent implements OnInit {
   get f(){
     return this.form.controls;
   }
+
+  //using onSubmit() function to display the values in the console
   onSubmit()
   {
     console.log(this.form)
     console.log(this.selectGender)
     console.log(this.selectTypes)
     this.res.res.push(this.form.value)
+    //the above line is written to access the values in next components.
     console.log(this.res.res)
     this.form.reset()
     console.log(this.afuConfig)
     
   }
+
+  //uploading this into slack using API
   afuConfig = {
     uploadAPI: {
       url:"https://slack.com/api/files.upload"
